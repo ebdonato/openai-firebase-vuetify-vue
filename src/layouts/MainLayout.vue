a070760c27589ada6ec1a843cb1d59a07b84138e
Eduardo Donato <eduardo.donato@gmail.com>
2023-02-01 02:50:12 -0300

    refactor app bar

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -18,17 +18,25 @@ const onLogout = () => {
 
 <template>
     <v-app-bar flat>
+        <template v-slot:prepend>
+            <v-btn icon variant="plain" :to="{ name: 'MainPage' }">
+                <v-avatar>
+                    <v-img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John"></v-img>
+                </v-avatar>
+            </v-btn>
+        </template>
         <v-app-bar-title>OpenAI Firebase Vuetify</v-app-bar-title>
-        <v-spacer></v-spacer>
-        <v-btn v-if="xs" color="primary" icon @click="toggleTheme">
-            <v-icon>{{ themeIcon }}</v-icon>
-        </v-btn>
-        <v-btn v-else color="primary" :prepend-icon="themeIcon" @click="toggleTheme"> Toggle Theme </v-btn>
-        <v-btn v-if="xs" color="primary" icon @click="onLogout">
-            <v-icon>mdi-logout</v-icon>
-        </v-btn>
-        <v-btn v-else color="primary" prepend-icon="mdi-logout" @click="onLogout"> Logout </v-btn>
-        <v-btn variant="flat" color="primary" class="ml-1" :to="{ name: 'CreatePage' }"> Create </v-btn>
+        <template v-slot:append>
+            <v-btn v-if="xs" color="primary" icon @click="toggleTheme">
+                <v-icon>{{ themeIcon }}</v-icon>
+            </v-btn>
+            <v-btn v-else color="primary" :prepend-icon="themeIcon" @click="toggleTheme"> Toggle Theme </v-btn>
+            <v-btn v-if="xs" color="primary" icon @click="onLogout">
+                <v-icon>mdi-logout</v-icon>
+            </v-btn>
+            <v-btn v-else color="primary" prepend-icon="mdi-logout" @click="onLogout"> Logout </v-btn>
+            <v-btn variant="flat" color="primary" class="ml-1" :to="{ name: 'CreatePage' }"> Create </v-btn>
+        </template>
     </v-app-bar>
     <RouterView />
 </template>
